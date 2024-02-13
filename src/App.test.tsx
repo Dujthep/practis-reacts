3584dd046b1d586cc76990db9c0e26848c06fc0b
dujthep-odds <dujthep@odds.team>
2024-02-13 14:51:48 +0700

    fix text

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import App from './App';
-
 describe('Expect test App service', () => {
   it('should be true', () => {
-    <App />;
+    // <App />;
     expect(true).toBeTruthy();
   });
 });
